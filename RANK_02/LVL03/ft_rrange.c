b6975f4441ebafabe19f0126168f956a71bce4e4
rickymercury <ricardomedeirosx@gmail.com>
2025-06-26 16:26:52 +0100

    add ft_rrange.c

---
+++ RANK_02/LVL03/ft_rrange.c
@@ -0,0 +1,52 @@
+/*Assignment name  : ft_rrange
+Expected files   : ft_rrange.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_rrange(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at end and end at start (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 3, 2 and 1
+- With (-1, 2) you will return an array containing 2, 1, 0 and -1.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing -3, -2, -1 and 0. */
+
+#include <stdlib.h>
+
+int ft_abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
+
+int *ft_rrange(int start, int end)
+{
+	int i = 0;
+	int *tab;
+
+	tab = malloc(sizeof(int) * (ft_abs(end - start) + 1));
+	if (!tab)
+		return (NULL);
+	while (start < end)
+	{
+		tab[i] = end;
+		end--;
+		i++;
+	}
+	while (start > end)
+	{
+		tab[i] = end;
+		end++;
+		i++;
+	}
+	tab[i] = end;
+	return (tab);
+}
