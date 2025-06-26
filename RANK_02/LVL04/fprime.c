ad90ff32f0575dc2434968f5159af91685c79668
rickymercury <ricardomedeirosx@gmail.com>
2025-06-26 17:28:21 +0100

    add fprime.c

---
+++ RANK_02/LVL04/fprime.c
@@ -0,0 +1,64 @@
+/*Assignment name  : fprime
+Expected files   : fprime.c
+Allowed functions: printf, atoi
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive int and displays its prime factors on the
+standard output, followed by a newline.
+
+Factors must be displayed in ascending order and separated by '*', so that
+the expression in the output gives the right result.
+
+If the number of parameters is not 1, simply display a newline.
+
+The input, when there is one, will be valid.
+
+Examples:
+
+$> ./fprime 225225 | cat -e
+3*3*5*5*7*11*13$
+$> ./fprime 8333325 | cat -e
+3*3*5*5*7*11*13*37$
+$> ./fprime 9539 | cat -e
+9539$
+$> ./fprime 804577 | cat -e
+804577$
+$> ./fprime 42 | cat -e
+2*3*7$
+$> ./fprime 1 | cat -e
+1$
+$> ./fprime | cat -e
+$
+$> ./fprime 42 21 | cat -e
+$*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+	int i = 2;
+	int num;
+	
+	if (ac == 2)
+	{
+		num = atoi(av[1]);
+		if (num == 1)
+			printf("1");
+		while (num > 1)
+		{
+			if (num % i == 0)
+			{
+				printf("%d", i);
+				num /= i;
+				if (num > 1)
+					printf ("*");
+			}
+			else
+				i++;
+		}
+	}
+	printf("\n");
+	return (0);
+}
